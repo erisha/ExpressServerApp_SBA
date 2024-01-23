c4a5ee65e844c363944f5eb3d43011a09e9b915b
Erisha Roach <forever.erisha@gmail.com>
2024-01-22 23:56:19 -0500

    added route to index.js Finished user route working on post and comments

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const morgan = require('morgan')
-
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/posts');
+const commentRoutes = require('./routes/comments');
 const app = express();
 const port = 3000;
 
 // middleware
 // morgan - request logging
  app.use(morgan('dev')); 
+
  app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
  });
 
+ 
 
+// Routes
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
+app.use('/comments', commentRoutes);
 
 
 
---
+++ routes/user.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router();
+
+// GET 
+router.get('/', (req,res) => res.json(users));
+
+// POST - create user
+router.post('/', (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    res.status(201).json(newUser)// successfully created newUser
+    res.json({message: `${newUser} has been successfully created`});
+})
+
+
+// PUT - update user /users/:id
+router.put('/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    res.json({message: `User ID: ${userId} updated`});
+});
+
+// DELETE /users/:id
+router.delete('/:id', (req,res) => {
+    const userId = parseInt(req.params.id);
+    res.json({message: `User ID: ${userId} has been deleted`});
+});
+
+module.exports = router;
