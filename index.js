4c547c9df3a09ef7762f1380dee78d7974dedaf4
Erisha Roach <forever.erisha@gmail.com>
2024-01-22 21:48:47 -0500

    added morgan middleware

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 const port = 3000;
 
 // middleware
+// morgan - request logging
+ app.use(morgan('dev')); 
+ app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+ });
 
-// app.use(morgan('dev'))
+
+
+
 
 
 
