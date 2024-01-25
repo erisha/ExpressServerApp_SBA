9605501a1da1b9beb09fbfeac7307dbb9ab7253d
Erisha Roach <forever.erisha@gmail.com>
2024-01-25 09:15:14 -0500

    .

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const morgan = require('morgan')
+const bodyParser = require ('body-parser');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/posts');
 const commentRoutes = require('./routes/comments');
+
 const app = express();
 const port = 3000;
 
@@ -14,14 +17,31 @@ const port = 3000;
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
  });
+
+ app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 //error handling
  app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong')
  });
 
+ // Views
+ app.set('view engine', 'ejs');
+ app.set('views', __dirname + '/views')
+
+// render view w/ form
+app.get('/', (req, res) => {
+    res.render('addPost');
+});
 
 // Routes
+let users = [];
+let posts = []; 
+let comments = [];
+
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
